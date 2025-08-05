d678e15cfd83f38b504e580af6f3ad89ed98016d
Malte Rettberg <malte.rettberg@web.de>
2025-08-05 15:12:22 +0200

    added render-function

--- script.js
+++ script.js
@@ -9,5 +9,16 @@ function init() {
 async function loadData(path = "") {
   let response = await fetch(BASE_URL + path + ".json");
   allNames = await response.json();
-  console.log(allNames);
+  console.log(allNames.results);
+  renderPkmCards(allNames.results);
+}
+
+function renderPkmCards(allNames) {
+  let pkmCard = document.getElementById("poke_cards");
+  //let allNames = allNames.results;
+  for (let indexPkm = 0; indexPkm < allNames.length; indexPkm++) {
+    pkmCard.innerHTML += `
+    <div><button onclick="showOverlay(${indexPkm})" id="pkm_details${indexPkm}">${allNames[indexPkm].name}</button></div>
+    `;
+  }
 }
